20ae2467f46a4852c0e1806d07e42fbae54b7217
Pavan Mantha <manthapavankumar11@gmail.com>
2022-11-30 23:06:59 +0530

    added more concepts like cross-refs, count&count-index, conditional-exps

---
+++ certified-terraform-associate/cross-references/reference.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIA4AQO2XMT2QIN5RTB"
+  secret_key = "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"
+}
+
+resource "aws_eip" "test_eip" {
+  vpc = true
+}
+
+resource "aws_instance" "test_ec2" {
+  ami = "ami-0b0dcb5067f052a63" //Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
+  instance_type = "t2.micro"
+}
+
+resource "aws_eip_association" "eip_ec2_mapping" {
+  instance_id = aws_instance.test_ec2.id
+  allocation_id = aws_eip.test_eip.id
+}
