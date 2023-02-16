606d8ca1e6a8c5f4a5822f02eb8deade4f9e6511
Pavan Mantha <manthapavankumar11@gmail.com>
2023-02-16 14:29:54 +0530

    terraform script for azure event hubs

---
+++ terraform-samples-azure/azure-eventhub/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm",
+        version = "3.43.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "eventhub_rg" {
+    name = var.rg_name
+    location = "West Europe"
+}
+
+resource "azurerm_eventhub_namespace" "eventhub_ns" {
+  name                = var.ns_name
+  location            = azurerm_resource_group.eventhub_rg.location
+  resource_group_name = azurerm_resource_group.eventhub_rg.name
+  sku                 = "Standard"
+  capacity            = 1
+  tags = {
+    environment = "development"
+  }
+}
+
+resource "azurerm_eventhub" "eventhub" {
+  name                = var.eventhub_name
+  namespace_name      = azurerm_eventhub_namespace.eventhub_ns.name
+  resource_group_name = azurerm_resource_group.eventhub_rg.name
+  partition_count     = 2
+  message_retention   = 1
+}
---
+++ terraform-samples-azure/azure-eventhub/variables.tf
@@ -0,0 +1,11 @@
+variable "rg_name" {
+  type = string
+}
+
+variable "ns_name" {
+  type = string
+}
+
+variable "eventhub_name" {
+  type = string
+}
