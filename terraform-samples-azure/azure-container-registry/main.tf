c5e6090fcbf568af52f565ab89df577a9c134931
Pavan Mantha <manthapavankumar11@gmail.com>
2023-02-18 23:06:17 +0530

    terraform script for azure container repository for storing docker images

---
+++ terraform-samples-azure/azure-container-registry/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "3.43.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    
+  }
+}
+
+resource "azurerm_resource_group" "devops-rg" {
+  name = var.devops-rg-name
+  location = var.devops-rg-location
+}
+
+resource "azurerm_container_registry" "acr" {
+    name = var.acr-name
+    resource_group_name = azurerm_resource_group.devops-rg.name
+    location = azurerm_resource_group.devops-rg.location
+    sku = var.acr-sku
+    admin_enabled = true
+}
---
+++ terraform-samples-azure/azure-container-registry/variables.tf
@@ -0,0 +1,15 @@
+variable "devops-rg-name" {
+  type = string
+}
+
+variable "devops-rg-location" {
+  type = string
+}
+
+variable "acr-name" {
+  type = string
+}
+
+variable "acr-sku" {
+  type = string
+}
