48b46144325ac7a99afb95e3f6940cbe177b11ee
Pavan Mantha <manthapavankumar11@gmail.com>
2023-10-17 22:44:27 +0530

    modified the provider version

--- terraform-samples-azure/azure-vault/main.tf
+++ terraform-samples-azure/azure-vault/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=3.37.0"
+      version = ">=3.75.0"
     }
   }
 }
@@ -18,15 +18,15 @@ provider "azurerm" {
 
 data "azurerm_client_config" "current" {}
 
-resource "azurerm_resource_group" "vault-rg" {
+resource "azurerm_resource_group" "ai102-rg" {
   name     = var.vault-rg-name
   location = var.location
 }
 
 resource "azurerm_key_vault" "my-vault" {
   name                        = var.vault-name
-  location                    = azurerm_resource_group.vault-rg.location
-  resource_group_name         = azurerm_resource_group.vault-rg.name
+  location                    = azurerm_resource_group.ai102-rg.location
+  resource_group_name         = azurerm_resource_group.ai102-rg.name
   enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
@@ -42,19 +42,4 @@ resource "azurerm_key_vault" "my-vault" {
     secret_permissions  = var.secret-permissions
     storage_permissions = var.storage-permissions
   }
-}
-
-resource "azurerm_key_vault_key" "public-key" {
-  name         = var.publickey-name
-  key_vault_id = azurerm_key_vault.my-vault.id
-  key_type     = "RSA"
-  key_size     = 4096
-
-  key_opts = var.publickey-opts
-}
-
-resource "azurerm_key_vault_secret" "example" {
-  name         = "tf-secret"
-  value        = "TU6p4UJ&49.5"
-  key_vault_id = azurerm_key_vault.my-vault.id
 }
